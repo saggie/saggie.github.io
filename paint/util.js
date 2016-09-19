5535db2720da83d45c27da702ebfd48769854ad9
saggie <sagiyama2010@gmail.com>
2016-09-20 00:33:33 +0900

    implement Bresenham's line algorithm

--- paint/util.js
+++ paint/util.js
@@ -11,6 +11,26 @@ var UTIL = {
     return input < min ? min : input > max ? max : input;
   },
   
+  // Bresenham's line algorithm
+  getCoordinatesBetween: function(x0, y0, x1, y1) {
+    var dx = Math.abs(x1 - x0);
+    var dy = Math.abs(y1 - y0);
+    var sx = (x0 < x1) ? 1 : -1;
+    var sy = (y0 < y1) ? 1 : -1;
+    var err = dx - dy;
+    
+    var ret = [x0, y0];
+    
+    while (!((x0 == x1) && (y0 == y1))) {
+      var e2 = err << 1;
+      if (e2 > -dy) { err -= dy; x0 += sx; }
+      if (e2 < dx) { err += dx; y0 += sy; }
+      ret.push(x0);
+      ret.push(y0);
+    }
+    return ret;
+  },
+  
   drawString: function(g, col, text, x, y) {
     g.fillStyle = col;
     g.fillText(text, x, y);
